5a2d6cb105d59bc481133aaaae0b8c3e148b71f4
jyl <kelvin94@live.cn>
2019-07-20 22:43:26 -0700

    more configs added

---
+++ src/main/java/com/jyl/authapi/authapi/AuthapiApplication.java
@@ -0,0 +1,15 @@
+package com.jyl.authapi.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AuthapiApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(AuthapiApplication.class, args);
+        }
+
+
+}
