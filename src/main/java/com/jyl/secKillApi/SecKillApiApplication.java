3c8aed9844932be3d4415c0898f49518a460d1a5
Kelvin Lin <kelvin94@live.cn>
2019-09-14 10:26:49 -0700

    init secKill repo

---
+++ src/main/java/com/jyl/secKillApi/SecKillApiApplication.java
@@ -0,0 +1,13 @@
+package com.jyl.secKillApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecKillApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecKillApiApplication.class, args);
+	}
+
+}
