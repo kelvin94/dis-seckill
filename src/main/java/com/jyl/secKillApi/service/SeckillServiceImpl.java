cb447c4f4a03f1ec2f94f99f04ff57f14283f0b8
kelvin94 <kelvin94@live.cn>
2019-09-28 17:14:21 -0700

    Fix a issue of "User gets md5 hashed url from backend during promoption period, and keep it, use it in the future."

--- src/main/java/com/jyl/secKillApi/service/SeckillServiceImpl.java
+++ src/main/java/com/jyl/secKillApi/service/SeckillServiceImpl.java
@@ -92,46 +92,47 @@ public class SeckillServiceImpl implements SeckillService {
         }
 
         try {
+            Optional<SeckillSwag> swag = swagRepository.findBySeckillSwagId(seckillSwagId);
 
-            Long remainingStockCount = swagRepository.findRemainingStock(seckillSwagId);
-            logger.debug("# 剩余库存 " + remainingStockCount);
-            if (remainingStockCount <= 0) {
-                throw new SeckillCloseException("卖完啦洗洗睡吧.");
-            } else {
-                // 减库存
-                remainingStockCount -= 1;
-                int updatedRows = swagRepository.updateStockCount(remainingStockCount, seckillSwagId);
-                logger.debug("#完成更新库存");
-                if (updatedRows != 1) {
-                    throw new Exception("Somethings wrong...Updated more than 1 swag's stock_count.");
-                }
-                Optional<SeckillSwag> swag = swagRepository.findBySeckillSwagId(seckillSwagId);
-                if(swag.isPresent()) {
-                    int state = 1;
-                        try {
-                            orderRepository.insertOder(swag.get().getSeckillSwagId(), swag.get().getSeckill_price(), userPhone, state);
-                            logger.debug("######finish insertion");
-                        }  catch( DataIntegrityViolationException ex) {
-                            logger.debug("######get into DataIntegrityViolationException");
-                            throw new RepeatkillException(Objects.requireNonNull(SeckillStateEnum.stateOf(-1)).getStateInfo());
-                        }
-
-                        return new SeckillExecution(swag.get().getSeckillSwagId(), 1, Objects.requireNonNull(SeckillStateEnum.stateOf(1)).getStateInfo());
-
-//                    SeckillOrder order = new SeckillOrder();
-//                    SeckillOrderPrimaryKey orderId = new SeckillOrderPrimaryKey();
-//                    orderId.setSeckillSwagId(swag.get().getSeckillSwagId());
-//                    orderId.setUserPhone(userPhone);
-//                    order.setOrderId(orderId);
-////                    order.setSeckillSwagId(swag.get().getSeckillSwagId());
-//                    order.setTotal(swag.get().getSeckill_price());
-////                    order.setUserPhone(userPhone);
-//                    order.setState(state);
-//                    SeckillOrder returnedInstance = orderRepository.save(order);
-
+            Date currentSysTime = new Date();
+            if(swag.isPresent() && currentSysTime.compareTo(swag.get().getStartTime()) > 0 && currentSysTime.compareTo(swag.get().getEndTime()) < 0 ) {
+                logger.debug("# get into the if statement (should NOT see this)");
+                long remainingStockCount = swag.get().getStockCount();
+                logger.debug("# 剩余库存 " + remainingStockCount);
+                if (remainingStockCount <= 0) {
+                    throw new SeckillCloseException("卖完啦洗洗睡吧.");
                 } else {
-                    throw new SeckillException("swag is not found");
+                    // 减库存
+                    remainingStockCount -= 1;
+                    int updatedRows = swagRepository.updateStockCount(remainingStockCount, seckillSwagId);
+                    logger.debug("#完成更新库存");
+                    if (updatedRows != 1) {
+                        throw new Exception("Somethings wrong...Updated more than 1 swag's stock_count.");
+                    }
+                    int state = 1;
+                    try {
+                        orderRepository.insertOder(swag.get().getSeckillSwagId(), swag.get().getSeckill_price(), userPhone, state);
+                        logger.debug("######finish insertion");
+                    }  catch( DataIntegrityViolationException ex) {
+                        logger.debug("######get into DataIntegrityViolationException");
+                        throw new RepeatkillException(Objects.requireNonNull(SeckillStateEnum.stateOf(-1)).getStateInfo());
+                    }
+
+                    return new SeckillExecution(swag.get().getSeckillSwagId(), 1, Objects.requireNonNull(SeckillStateEnum.stateOf(1)).getStateInfo());
+
+                    //                    SeckillOrder order = new SeckillOrder();
+                    //                    SeckillOrderPrimaryKey orderId = new SeckillOrderPrimaryKey();
+                    //                    orderId.setSeckillSwagId(swag.get().getSeckillSwagId());
+                    //                    orderId.setUserPhone(userPhone);
+                    //                    order.setOrderId(orderId);
+                    ////                    order.setSeckillSwagId(swag.get().getSeckillSwagId());
+                    //                    order.setTotal(swag.get().getSeckill_price());
+                    ////                    order.setUserPhone(userPhone);
+                    //                    order.setState(state);
+                    //                    SeckillOrder returnedInstance = orderRepository.save(order);
+
                 }
+
             }
 
         } catch (RepeatkillException ex) {
@@ -144,5 +145,7 @@ public class SeckillServiceImpl implements SeckillService {
             logger.error("##userPhone: " + userPhone + " " + ex.getMessage());
             throw new SeckillException(ex.getMessage());
         }
+        logger.debug("# return null (should see this)");
+        return null;
     }
 }
