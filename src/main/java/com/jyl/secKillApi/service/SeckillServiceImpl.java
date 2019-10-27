87740448fd29729492193d9448988d7c34a7f992
Kelvin Lin <kelvin94@live.cn>
2019-10-27 10:38:07 -0700

    separated business logic into appropriate service

--- src/main/java/com/jyl/secKillApi/service/SeckillServiceImpl.java
+++ src/main/java/com/jyl/secKillApi/service/SeckillServiceImpl.java
@@ -3,25 +3,25 @@ package com.jyl.secKillApi.service;
 import com.google.gson.Gson;
 import com.jyl.secKillApi.dto.SeckillExecution;
 import com.jyl.secKillApi.dto.UrlExposer;
-import com.jyl.secKillApi.entity.SeckillOrder;
 import com.jyl.secKillApi.entity.SeckillSwag;
 import com.jyl.secKillApi.execptions.RepeatkillException;
 import com.jyl.secKillApi.execptions.SeckillCloseException;
 import com.jyl.secKillApi.execptions.SeckillException;
+import com.jyl.secKillApi.rabbitmq.MQProducer;
 import com.jyl.secKillApi.repository.OrderRepository;
 import com.jyl.secKillApi.repository.SwagRepository;
+import com.jyl.secKillApi.resource.SeckillMsgBody;
 import com.jyl.secKillApi.resource.SeckillParameter;
 import com.jyl.secKillApi.stateenum.SeckillStateEnum;
+import com.jyl.secKillApi.util.GeneralUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -32,19 +32,20 @@ public class SeckillServiceImpl implements SeckillService {
     private static Logger logger = LogManager.getLogger(SeckillServiceImpl.class.getSimpleName());
     private final Gson gson;
     private final SwagRepository swagRepository;
-    private final OrderRepository orderRepository;
+    private final MQProducer mqProducer;
     private final JedisPool jedisPool;
     private final String salt = "randomsalt555";
     public SeckillServiceImpl(
             SwagRepository swagRepository,
             OrderRepository orderRepository,
             JedisPool jedisPool,
-            Gson gson
+            Gson gson,
+            MQProducer mqProducer
     ) {
         this.swagRepository = swagRepository;
-        this.orderRepository = orderRepository;
         this.jedisPool = jedisPool;
         this.gson = gson;
+        this.mqProducer = mqProducer;
     }
 
     @Override
@@ -70,9 +71,9 @@ public class SeckillServiceImpl implements SeckillService {
         try {
             jedis = jedisPool.getResource();
             // check if the key(swag id) is in redis, if exists, decompose the json convert it back to POJO
-            if(jedis.exists(getUrlRedisKey(seckillSwagId))) {
+            if(jedis.exists(GeneralUtil.getUrlRedisKey(seckillSwagId))) {
                 logger.info("Seckill product exists in Redis. Returning obj in redis.");
-                String redis_value = jedis.get(getUrlRedisKey(seckillSwagId));
+                String redis_value = jedis.get(GeneralUtil.getUrlRedisKey(seckillSwagId));
                 return gson.fromJson(redis_value, UrlExposer.class);
             }
         } catch (Exception ex) {
@@ -109,7 +110,7 @@ public class SeckillServiceImpl implements SeckillService {
                 String str_returnValue = gson.toJson(returnValue);
                 try{
                     jedis = jedisPool.getResource();
-                    jedis.set(getUrlRedisKey(seckillSwagId), str_returnValue);
+                    jedis.set(GeneralUtil.getUrlRedisKey(seckillSwagId), str_returnValue);
                 } catch (Exception ex) {
                     logger.error(ex.getMessage());
                 } finally {
@@ -124,15 +125,6 @@ public class SeckillServiceImpl implements SeckillService {
         return null;
     }
 
-    private String getUrlRedisKey(Long seckillSwagId) {
-        return "swagUrl:"+seckillSwagId;
-    }
-
-
-
-    private String getSeckillOrderRedisKey(Long userPhone, Long seckillSwagId) {
-        return "swagOrder:"+seckillSwagId+":"+userPhone;
-    }
 
     private String getMd5(long seckillSwagId) {
         String base = seckillSwagId + "/" + seckillSwagId;
@@ -157,72 +149,29 @@ public class SeckillServiceImpl implements SeckillService {
             String str_order = null;
             try {
                 jedis = jedisPool.getResource();
-                str_order = jedis.get(getSeckillOrderRedisKey(userPhone, seckillSwagId));
-                logger.info("完成从redis读取SeckillOrder: "+str_order);
+                str_order = jedis.get(GeneralUtil.getSeckillOrderRedisKey(userPhone, seckillSwagId));
             } finally {
                 if(jedis!=null) {
                     logger.info("Redis conn close");
                     jedis.close();
                 }
             }
-            long remainingStockCount = 0;
-            BigDecimal seckill_price = null;
-            long dealStartTs = 0;
-            long dealEndTs = 0;
 
             if(str_order != null) {
-                SeckillOrder order = gson.fromJson(str_order, SeckillOrder.class);
                 logger.info("Seckill order exists in redis. 重复购买。");
                 throw new RepeatkillException("Your order already placed.");
             } else {
-                try {
-                    // Non-repeated purchase - Update Redis Url with new stockCount
-                    jedis = jedisPool.getResource();
-                    String seckill_url = jedis.get(getUrlRedisKey(seckillSwagId));
-                    UrlExposer url = gson.fromJson(seckill_url, UrlExposer.class);
-                    if(url != null && url.getStockCount() > 0) {
-                        url.setStockCount(url.getStockCount() - 1 );
-                        remainingStockCount = url.getStockCount();
-                        dealStartTs = url.getDealStart();
-                        dealEndTs = url.getDealEnd();
-                        seckill_price = url.getSeckill_price();
-                    }
-                } finally {
-                    if(jedis!=null)
-                        jedis.close();
-                }
-                logger.info("剩余库存 " + remainingStockCount);
-                logger.info("(Redis) 完成更新库存");
-                if (remainingStockCount <= 0) {
-                    throw new SeckillCloseException("Sold out. 卖完啦洗洗睡吧.");
-                }
-                Date currentSysTime = new Date();
-                if(currentSysTime.getTime() > dealStartTs && currentSysTime.getTime() < dealEndTs ) {
 
-                    // 减库存
-                    int updatedRows = swagRepository.updateStockCount(remainingStockCount, seckillSwagId);
-                    logger.info("(Postgres) 完成更新库存");
-                    if (updatedRows != 1) {
-                        throw new Exception("Somethings wrong...Updated more than 1 swag's stock_count.");
-                    }
-                    int state = 1;
-                    try {
-                        jedis = jedisPool.getResource();
-                        SeckillOrder order = new SeckillOrder(seckillSwagId, seckill_price, userPhone, state);
-                        jedis.set(getSeckillOrderRedisKey(userPhone, seckillSwagId), gson.toJson(order));
-                        logger.info("(Redis) 更新order完笔");
-                        orderRepository.insertOder(seckillSwagId, seckill_price, userPhone, state);
-                        logger.info("(Postgres) 更新Order完毕");
-                    }  catch( DataIntegrityViolationException ex) {
-                        logger.error(ex.getMessage());
-                        throw new RepeatkillException(Objects.requireNonNull(SeckillStateEnum.stateOf(-1)).getStateInfo());
-                    } finally {
-                        if(jedis!=null)
-                            jedis.close();
-                    }
+                    long threadId = Thread.currentThread().getId();
+                /*
+                    2019-Oct-26 Update: encapsulate swagID + userPhone as a msg, send the msg to the jianku_exchange
+                 */
+                SeckillMsgBody msg = new SeckillMsgBody(seckillSwagId, userPhone);
+                    // // 进入待秒杀队列，进行后续串行操作
+                    mqProducer.jianku_send(msg);
 
                     return new SeckillExecution(seckillSwagId, 1, Objects.requireNonNull(SeckillStateEnum.stateOf(1)).getStateInfo());
-                }
+//                }
 
             }
 
@@ -236,6 +185,5 @@ public class SeckillServiceImpl implements SeckillService {
             logger.error("##userPhone: " + userPhone + " " + ex.getMessage());
             throw new SeckillException(ex.getMessage());
         }
-        return null;
     }
 }
