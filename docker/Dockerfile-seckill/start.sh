2e6994992ec1a99083d3d2b5d38aa23386b24798
kelvin94 <kelvin94@live.cn>
2019-12-03 07:20:29 -0800

    Dockerized entire local dev environment. Lesson learned: need to bind the hostname of the box that runs redis to tell Redis listening for traffic coming from network interface "redis-container", which is ramdon private ip address assigned by docker

--- docker/Dockerfile-seckill/start.sh
+++ docker/Dockerfile-seckill/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+echo "changing /tmp permission..." >> /tmp/log/bash-scripts-logs/start-logs.log
+chmod +x /tmp/
+echo "connecting to rabbitmq container..." >> /tmp/log/bash-scripts-logs/start-logs.log
 while ! nc -z rabbitmq-container 5672; do sleep 3; done
+echo "Done" >> /tmp/log/bash-scripts-logs/start-logs.log
+echo "connecting to redis container..." >> /tmp/log/bash-scripts-logs/start-logs.log
 while ! nc -z redis-container 6379; do sleep 3; done
-
+echo "Done" >> /tmp/log/bash-scripts-logs/start-logs.log
 bash /rabbitmq-init.sh
